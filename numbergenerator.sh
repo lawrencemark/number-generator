16467c6963e16c76db19cd4fd8e7384d826958ce
lawrencemark <marklawrence@hotmail.co.uk>
2023-08-26 21:04:35 +0100

    Script Creation

--- numbergenerator.sh
+++ numbergenerator.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 python3 phonenumbergen.py $1
-echo ill go and run something else 
+
+case $1 in 
+
+    UK|uk)
+
+    toNumber="+44800123456"
+    ;;
+
+    FRANCE|France|france)
+
+    toNumber="+33147777000"
+    ;;
+
+    GERMANY|Germany|germany)
+
+    toNumber="+496997971000"
+    ;;
+
+    NETHERLANDS|Netherlands:netherlands)
+    toNumber="+31205048000"
+    ;;
+
+    SPAIN|Spain|spain)
+    toNumber="+34902375637"
+    ;;
+
+esac
+
+sed "s/TONUMBER/$toNumber/g" sipp_uac_pcap_g711a.template > sipp_uac_invitewithrtp.xml
+
+sudo sipp 192.168.244.128 -sf sipp_uac_invitewithrtp.xml -inf numbers.csv -r 3 -rp 30000 -l 250 -i 192.168.244.129
+
+
