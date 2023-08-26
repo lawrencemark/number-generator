50a48e2e6fbf6b81fbb6104a401d7343de436029
lawrencemark <marklawrence@hotmail.co.uk>
2023-08-26 17:52:00 +0100

    if to CASE

--- phonenumbergen.py
+++ phonenumbergen.py
@@ -11,25 +11,32 @@ import random
 import sys
 
 try:
-    country = str(sys.argv[1])  
+    country = str(sys.argv[1]).upper()  
 except:
     print('Please set country argument on the command line.')
-    print('Example: phonenumbergen.py UK')
+    print('Example: phonenumbergen.py UK, NETHERLANDS, SPAIN, FRANCE OR GERMANY')
     quit()
 
-if country == 'germany' or country == 'Germany' or country == 'GERMANY':
-    noLength = 12
-    areaCodes = [4930,49521,49234,49421,49231,49351,49203,49211,49201,4969,4940,49511,49221,49341,4989,49911,49711,49202]
-elif country == 'uk' or country == 'UK':
-    noLength = 12
-    areaCodes = [441273,441444,441903,44207,44161,44205,4479,441771,441785,441804,441908]
-elif country == 'france' or country == 'France' or country == 'FRANCE':
-    noLength = 11
-    areaCodes = [331,332,333,334,335,336,337,338,339]
-else:
-    print('Please set a valid country argument on the command line.')
-    print('Example: phonenumbergen.py UK')
-    quit()
+match country:
+    case 'GERMANY':
+        noLength = 12
+        areaCodes = [4930,49521,49234,49421,49231,49351,49203,49211,49201,4969,4940,49511,49221,49341,4989,49911,49711,49202]
+    case 'UK':
+        noLength = 12
+        areaCodes = [441273,441444,441903,44207,44161,44205,4479,441771,441785,441804,441908]
+    case 'FRANCE':
+        noLength = 11
+        areaCodes = [331,332,333,334,335,336,337,338,339,3110,3120,31223,31486,3168]
+    case 'NETHERLANDS':
+        noLength = 11
+        areaCodes = [31341,3138,31411,3145,31571,31524,]  
+    case 'SPAIN':
+        noLength = 11
+        areaCodes = [34934,34912,34927,34945,34949,34958,34960,3471,3476,3477]        
+    case _:
+        print('Please set a valid country argument on the command line.')
+        print('Example: phonenumbergen.py UK, NETHERLANDS, SPAIN, FRANCE OR GERMANY')
+        quit()
 
 def randomnumber(N):
 	minimum = pow(10, N-1)
